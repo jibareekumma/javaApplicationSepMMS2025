bbaa7f46b265242800c5561a5a61e8dff4820c4a
jibareekumma <shadowcelebekumma@gmail.com>
2026-05-06 15:54:19 +0100

    java class 1

---
+++ Hello.java
@@ -0,0 +1,10 @@
+
+
+public class Hello{
+	public static void main(String[] args){
+		System.out.println("Hello Everyone, i am learning java");
+		System.out.print("OOh wow, i am learning java");
+		System.out.print("Hurray, i am happy");
+	}
+	
+}
