536e6e01a97f6762953c5fa7adc357f075c4d0bb
jibareekumma <shadowcelebekumma@gmail.com>
2026-05-06 16:13:23 +0100

    updated

--- Hello.java
+++ Hello.java
@@ -5,6 +5,7 @@ public class Hello{
 		System.out.println("Hello Everyone, i am learning java");
 		System.out.print("OOh wow, i am learning java");
 		System.out.print("Hurray, i am happy");
+		System.out.printf("do you love java %c %b", '?', true);
 	}
 	
 }
